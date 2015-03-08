745dc992a19e7f3516caaaba2b17d7b6621ff4fc
Cyral <cyral@pyratron.com>
2015-03-08 18:12:01 -0400

    Remove unused stuff

--- Source/Parser/Properties/AssemblyInfo.cs
+++ Source/Parser/Properties/AssemblyInfo.cs
@@ -1,42 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("Command Parser")]
-[assembly:
-    AssemblyDescription(
-        "Simple, lightweight, but powerful command parsing library. Useful for command handling in chat or game applications."
-        )]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Simple, lightweight, but powerful command parsing library. Useful for command handling in chat or game applications.")]
 [assembly: AssemblyCompany("Pyratron Studios")]
 [assembly: AssemblyProduct("Command Parser")]
 [assembly: AssemblyCopyright("Copyright © 2015 Pyratron Studios")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("a8cce35f-dbe6-4ab6-b340-c10dc7c907e3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
